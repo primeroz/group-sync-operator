47489d4ec12168fe6cd441d6ff48af1c919d6b6c
Francesco Ciocchetti <primeroznl@gmail.com>
2023-10-25 12:40:35 +0200

    Add syncperiod

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ package main
 import (
 	"flag"
 	"os"
+	"time"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
@@ -52,8 +53,10 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var syncInterval int
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.IntVar(&syncInterval, "sync-interval", 30, "ReSync interval in seconds")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -65,11 +68,13 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
+	s := time.Second * time.Duration(syncInterval)
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
+		SyncPeriod:             &s,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "d43f3313.primeroz.xyz",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
